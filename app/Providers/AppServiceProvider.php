873370981bcd384e3985875e4bb55f2650c0a376
JohnPaulZer <your-johnpaulzer27@gmail.com>
2026-05-05 19:57:27 +0800

    fix: enhance boot method in AppServiceProvider for Windows compatibility

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Console\ServeCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,5 +18,34 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        if (PHP_OS_FAMILY !== 'Windows') {
+            return;
+        }
+
+        ServeCommand::$passthroughVariables = array_values(array_unique(array_merge(
+            ServeCommand::$passthroughVariables,
+            [
+                'APPDATA',
+                'COMSPEC',
+                'HOMEDRIVE',
+                'HOMEPATH',
+                'LOCALAPPDATA',
+                'PATH',
+                'Path',
+                'PATHEXT',
+                'ProgramData',
+                'ProgramFiles',
+                'ProgramFiles(x86)',
+                'SystemDrive',
+                'SystemRoot',
+                'TEMP',
+                'TMP',
+                'USERPROFILE',
+                'WINDIR',
+                'windir',
+            ]
+        )));
+    }
 }
